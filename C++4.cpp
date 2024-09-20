a37240d4a09138b95bfd49a29a7a11be82e4c6ed
Shubham-shaw-c <shubham.shaw@stu.adamasuniversity.ac.in>
2024-09-20 19:25:22 +0530

    Write a program in c to count Vowels and Consonants in a string using switch case.

---
+++ C++4.cpp
@@ -0,0 +1,29 @@
+//Wap in c to count Vowels and Consonat in a string using switchcase.
+#include <stdio.h>
+#include <ctype.h>
+int main() {
+    char str[100];
+    int vowels = 0, consonants = 0, i = 0;
+    printf("Enter a string: ");
+    gets(str);
+    while (str[i] != '\0') {
+        char ch = tolower(str[i]);
+        if ((ch >= 'a' && ch <= 'z')) {
+            switch (ch) {
+                case 'a':
+                case 'e':
+                case 'i':
+                case 'o':
+                case 'u':
+                    vowels++;
+                    break;
+                default:
+                    consonants++;
+                    break;
+            }
+        }
+        i++;
+    }
+    printf("Number of vowels: %d\n", vowels);
+    printf("Number of consonants: %d\n", consonants);
+}
