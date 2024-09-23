62fe7af5a407cc41259a4dc47f16aa58744d1c04
Shubham-shaw-c <shubham.shaw@stu.adamasuniversity.ac.in>
2024-09-23 18:51:03 +0530

    Write a program in c++ program to implement reverse floyd's triangle

---
+++ C++10.cpp
@@ -0,0 +1,15 @@
+//Write a program in c++ program to implement reverse floyd's triangle
+#include <iostream>
+using namespace std;
+int main() {
+    int rows, num = 1;
+    cout << "Enter the number of rows: ";
+    cin >> rows;
+    int totalNumbers = (rows * (rows + 1)) / 2;
+    for (int i = rows; i >= 1; i--) {
+        for (int j = 1; j <= i; j++) {
+            cout << totalNumbers-- << " ";
+        }
+        cout << endl;
+    }
+}
