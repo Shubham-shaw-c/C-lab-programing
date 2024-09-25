be7d0325294206aee54f05d09c023ad48b39c423
Shubham-shaw-c <shubham.shaw@stu.adamasuniversity.ac.in>
2024-09-25 22:02:56 +0530

    Write a program in c++ to calculate simple interest where the rate of interest has a default value of 15% .

---
+++ C++13.cpp
@@ -0,0 +1,17 @@
+//Write a program in c++ to calculate simple interest where the rate of interest has a default value of 15%
+#include<iostream>
+using namespace std;
+float calculateSimpleInterest(float principal, float time, float rate = 15.0) {
+    return (principal * rate * time) / 100;
+}
+
+int main() {
+    float principal, time, interest;
+    cout << "Enter the principal amount: ";
+    cin >> principal;
+    cout << "Enter the time (in years): ";
+    cin >> time;
+    interest = calculateSimpleInterest(principal, time);
+    cout << "The Simple Interest is: " << interest << endl;
+
+}
